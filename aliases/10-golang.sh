1d3af3d506c580b74dd41d761a5d8112a6d0ef22
Rodrigo Corrêa <rcelha@gmail.com>
2017-05-10 00:01:32 +0100

    Bump golang version

--- aliases/10-golang.sh
+++ aliases/10-golang.sh
@@ -1,5 +1,5 @@
 golang_install () {
-    sudo sh -c "rm -fr /opt/go && curl https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz | tar -C /opt -xvz"
+    sudo sh -c "rm -fr /opt/go && curl https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz | tar -C /opt -xvz"
 }
 
 export GOROOT=/opt/go
