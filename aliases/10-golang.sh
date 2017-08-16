452e92e0c22993fb15996b561dfdaef20775bc59
Rodrigo Corrêa <rcelha@gmail.com>
2017-08-16 16:17:41 +0100

    Install go in the home folder

--- aliases/10-golang.sh
+++ aliases/10-golang.sh
@@ -1,6 +1,9 @@
+BASE=~/opt
+export GOROOT=$BASE/go
+
 golang_install () {
-    sudo sh -c "rm -fr /opt/go && curl https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz | tar -C /opt -xvz"
+    mkdir -p $BASE;
+    sh -c "rm -fr /opt/go && curl https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | tar -C $BASE -xvz"
 }
 
-export GOROOT=/opt/go
 export PATH=$GOROOT/bin:$PATH
