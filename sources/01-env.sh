f509bc25adf62bdf25f23cd6ed34915878c62623
Rodrigo Corrêa <rcelha@gmail.com>
2017-11-22 23:46:22 +0000

    Add some installer

--- sources/01-env.sh
+++ sources/01-env.sh
@@ -2,3 +2,7 @@ export EDITOR=vim
 export PATH=$HOME/bin:$HOME/local/bin:$PATH
 export PATH=/home/rcelha/.yarn/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+
+export ANDROID_HOME=$HOME/.android-sdk
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/tools/bin:$PATH
