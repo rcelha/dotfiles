2422088989cbfb1b357da9e2d48027c3937cfa0e
Rodrigo Corrêa <rcelha@gmail.com>
2017-12-27 22:31:35 +0000

    Sanitize PATH var definition

--- sources/.sources/01-env.sh
+++ sources/.sources/01-env.sh
@@ -1,9 +1,13 @@
 export EDITOR=vim
-export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
-export PATH=$HOME/bin:$HOME/local/bin:$PATH
+
+export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.yarn/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+export PATH=$HOME/Library/Python/3.6/bin:$PATH
+export PATH=$HOME/local/bin:$PATH
+export PATH=$HOME/bin:$PATH
 
 export ANDROID_HOME=$HOME/.android-sdk
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/tools/bin:$PATH
+
+export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
