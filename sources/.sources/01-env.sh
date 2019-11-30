9232a72ff16a9612844669fb83031a204f05e0cc
Rodrigo Corrêa <rcelha@gmail.com>
2019-11-30 23:17:26 +0000

    Change android path

--- sources/.sources/01-env.sh
+++ sources/.sources/01-env.sh
@@ -6,8 +6,12 @@ export PATH=$HOME/Library/Python/3.6/bin:$PATH
 export PATH=$HOME/local/bin:$PATH
 export PATH=$HOME/bin:$PATH
 
-export ANDROID_HOME=$HOME/.android-sdk
-export PATH=$ANDROID_HOME/tools:$PATH
-export PATH=$ANDROID_HOME/tools/bin:$PATH
 
 export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+
+# export ANDROID_HOME=$HOME/.android-sdk
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
