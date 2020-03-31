bc1f5c1bd3d7f7187faf335022e8d81e4767862a
Rodrigo Corrêa <rcelha@gmail.com>
2020-03-31 15:46:34 +0100

    Replace mysql-client 8 with 5.7

---
+++ sources/.sources/10-mysql-client.sh
@@ -0,0 +1,4 @@
+export PATH="/usr/local/opt/mysql-client@5.7/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/mysql-client@5.7/lib"
+export CPPFLAGS="-I/usr/local/opt/mysql-client@5.7/include"
+export PKG_CONFIG_PATH="/usr/local/opt/mysql-client@5.7/lib/pkgconfig"
