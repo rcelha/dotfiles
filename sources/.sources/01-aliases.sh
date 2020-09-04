8c76e82fcbd637cb71464902c9ec956a4184173a
Rodrigo Corrêa <rcelha@gmail.com>
2020-09-04 16:45:29 +0100

    Fix macosx detection

--- sources/.sources/01-aliases.sh
+++ sources/.sources/01-aliases.sh
@@ -1,4 +1,4 @@
-if [ "$OSTYPE" = "darwin" ]
+if [ "$(uname -s)" = "Darwin" ]
 then
     alias ls="gls --color=auto"
 else
@@ -19,7 +19,7 @@ json_format () {
 }
 
 alert() {
-    if [ "$OSTYPE" = "darwin" ]
+    if [ "$(uname -s)" = "Darwin" ]
     then
         osascript -e 'display notification with title "'$1'"'
     else
@@ -27,7 +27,7 @@ alert() {
     fi
 }
 
-if [ "$OSTYPE" = "darwin" ]
+if [ "$(uname -s)" = "Darwin" ]
 then
     alias brave="open -a Brave\ Browser"
     alias chrome="open -a Google\ Chrome"
