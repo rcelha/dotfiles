1f4486d9c842a86b758c17db440f37c20cb51cb6
Rodrigo Corrêa <rcelha@gmail.com>
2021-05-02 21:50:24 +0100

    Enable docker on Fedora script

--- scripts/post-install-fedora34.sh
+++ scripts/post-install-fedora34.sh
@@ -32,6 +32,7 @@ sudo dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable --now docker
 
 
 # Brave
