67971f5153fed1a345aa24e2bd29bb5e87459164
Rodrigo Corrêa <rcelha@gmail.com>
2021-05-13 10:13:52 +0100

    Update logid config

--- sources/.sources/10-python.sh
+++ sources/.sources/10-python.sh
@@ -1,8 +1,9 @@
-export PATH="$HOME/.pyenv/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$HOME/.poetry/bin:$PATH"
 
 which pyenv > /dev/null 2>&1 && {
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 }
 
