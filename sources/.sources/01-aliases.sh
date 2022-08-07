ba6f5f6d7a1e698ac25d37ea4c5db104b05f5008
Rodrigo Corrêa <rcelha@gmail.com>
2022-08-07 23:16:57 +0100

    Add l and ll aliases

--- sources/.sources/01-aliases.sh
+++ sources/.sources/01-aliases.sh
@@ -4,6 +4,8 @@ alias ...="cd ../.."
 alias ..="cd .."
 alias grep="grep --exclude-dir=node_modules --color"
 alias json_format='python -m json.tool'
+alias l="ls -lh"
+alias ll="ls -lha"
 
 which nvim > /dev/null 2>&1 && {
     alias vim=nvim
