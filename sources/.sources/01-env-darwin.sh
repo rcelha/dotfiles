1d2574df2f49488118d61bcfb4cec93d4e2729bf
Rodrigo Corrêa <rcelha@gmail.com>
2023-03-01 16:19:51 +0000

    Add coc-pairs

--- sources/.sources/01-env-darwin.sh
+++ sources/.sources/01-env-darwin.sh
@@ -2,5 +2,6 @@ if [ $(uname -s) = "Darwin" ]
 then
     export PATH=/opt/homebrew/opt/redis@6.2/bin:$PATH
     export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+    export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 fi
 
