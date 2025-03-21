0094495c6ad3bf9f22dfbeb3e50e2df709f061eb
Rodrigo Corrêa <rcelha@gmail.com>
2025-03-21 17:51:14 +0000

    extract install from brewfile into script

--- scripts/post-install-mac.sh
+++ scripts/post-install-mac.sh
@@ -3,14 +3,15 @@
 set -ex
 
 export PATH=/opt/homebrew/bin/:$PATH
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
 
 # homebrew
 if [ ! `which brew` ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi;
 
-export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
-brew bundle
+
+./install-from-brewfile
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
