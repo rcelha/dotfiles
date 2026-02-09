4921ec086974cb0f128c31bc9d114de8a797efde
Rodrigo Corrêa <rcelha@gmail.com>
2026-02-09 11:18:48 +0000

    add bun config

--- sources/.sources/10-node.sh
+++ sources/.sources/10-node.sh
@@ -1,1 +1,8 @@
 [ -d ~/.fnm ] && export PATH=$PATH:~/.fnm && eval $(fnm env)
+
+# bun completions
+[ -s "/Users/rodrigo.alves/.bun/_bun" ] && source "/Users/rodrigo.alves/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
